cdbc21925cfa4421a575c9ecc6cc4e39b24b4abf
Rotno Chandra Das <rotno050@gmail.com>
2026-05-09 16:45:57 +0600

    Done

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,56 +1,8 @@
 import { RechartsDevtools } from '@recharts/devtools';
-import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Chart = ({data}) => {
     const appData = data;
-
-    console.log(appData);
-
-    // const appData = [
-    //     {
-    //         name: 'Page A',
-    //         uv: 4000,
-    //         pv: 2400,
-    //         amt: 2400,
-    //     },
-    //     {
-    //         name: 'Page B',
-    //         uv: 3000,
-    //         pv: 1398,
-    //         amt: 2210,
-    //     },
-    //     {
-    //         name: 'Page C',
-    //         uv: 2000,
-    //         pv: 9800,
-    //         amt: 2290,
-    //     },
-    //     {
-    //         name: 'Page D',
-    //         uv: 2780,
-    //         pv: 3908,
-    //         amt: 2000,
-    //     },
-    //     {
-    //         name: 'Page E',
-    //         uv: 1890,
-    //         pv: 4800,
-    //         amt: 2181,
-    //     },
-    //     {
-    //         name: 'Page F',
-    //         uv: 2390,
-    //         pv: 3800,
-    //         amt: 2500,
-    //     },
-    //     {
-    //         name: 'Page G',
-    //         uv: 3490,
-    //         pv: 4300,
-    //         amt: 2100,
-    //     },
-    // ];
-
     return (
         <BarChart
             style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
@@ -63,7 +15,6 @@ const Chart = ({data}) => {
                 bottom: 5,
             }}
         >
-            {/* <CartesianGrid strokeDasharray="3 3" /> */}
             <XAxis dataKey="name" />
             <YAxis width="auto" dataKey="count" />
             <Tooltip />
